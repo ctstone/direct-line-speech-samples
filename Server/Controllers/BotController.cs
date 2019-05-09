a2a188c0d5d5bd983ad126e280629ebe8fe20846
Chris Stone <chstone@microsoft.com>
2019-05-09 18:04:26 -0400

    Add project files.

---
+++ Server/Controllers/BotController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+
+namespace Server.Controllers
+{
+    [Route("api/messages")]
+    [ApiController]
+    public class BotController : Controller
+    {
+        private readonly IBotFrameworkHttpAdapter Adapter;
+        private readonly IBot Bot;
+
+        public BotController(IBotFrameworkHttpAdapter adapter, IBot bot)
+        {
+            Adapter = adapter;
+            Bot = bot;
+        }
+
+        [HttpPost, HttpGet]
+        public async Task Index()
+        {
+            await Adapter.ProcessAsync(Request, Response, Bot);
+        }
+    }
+}
