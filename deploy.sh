7d0062b15e21863af16692d5e5a78103b75dba0c
Chris Stone <chstone@microsoft.com>
2019-05-10 10:42:46 -0400

    update deploy steps

--- deploy.sh
+++ deploy.sh
@@ -7,12 +7,14 @@ ACR=$4
 WEB=$5
 PLAN=$6
 
+IMAGE=direct-line-speech-demo-server
+
 # BUILD CONTAINER IMAGE
 az acr build \
   --resource-group $RG \
   --registry $ACR \
-  --image direct-line-speech-demo-server:{{.Run.ID}} \
-  --image direct-line-speech-demo-server:latest \
+  --image $IMAGE:{{.Run.ID}} \
+  --image $IMAGE:latest \
   Server
   
 # CREATE WEB APP
@@ -21,3 +23,25 @@ az webapp create \
   --name $WEB \
   --plan $PLAN \
   --runtime 'aspnet|v4.7'
+  
+# CONFIGURE WEB APP SETTINGS
+az webapp config appsettings set \
+  --resource-group $RG \
+  --name $WEB \
+  --settings \
+    MSA_APP_ID=$MSA_APP_ID \
+    MSA_PASSWORD=$MSA_PASSWORD
+
+# CONFIGURE WEB APP CONTAINER IMAGE
+ACR_KEY=$(az acr credential show \
+  --resource-group $RG \
+  --name $ACR \
+  --query passwords[0].value \
+  --output tsv)
+az webapp config container set \
+  --resource-group $RG \
+  --name $WEB \
+  --docker-custom-image-name $ACR.azurecr.io/$IMAGE:latest \
+  --docker-registry-server-password $ACR_KEY \
+  --docker-registry-server-url https://$ACR.azurecr.io \
+  --docker-registry-server-user $ACR
